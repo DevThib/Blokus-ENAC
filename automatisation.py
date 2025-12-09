a0b6eadf98b3d9cada141eb34ee98a8c58fbe671
collecnoe <collec.noe@gmail.com>
2025-12-09 15:17:02 +0100

    Add files via upload

--- automatisation.py
+++ automatisation.py
@@ -12,46 +12,52 @@ def retournement(piece):
 def versions(piece):
     v1=retournement(piece)
     D={1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
-    p=piece
-    D[1]=piece[1]
-    v=v1[1]
-    D[2]=v
+    p=piece.copy()
+    D[1]=p[1]
+    v=v1.copy()
+    D[2]=v[1]
     for i in range(len(p[1])):
-        a,b=p[1][i]
+        p1=piece.copy()
+        a,b=p1[1][i]
         a=b
         b=-a
-        p[1][i]=(a,b)
-    D[3]=p[1]
-    for i in range(len(p[1])):
-        a,b=p[1][i]
+        p1[1][i]=(a,b)
+    D[3]=p1[1]
+    for i in range(len(piece[1])):
+        p2=piece.copy()
+        a,b=p2[1][i]
         a=-a
         b=-b
-        p[1][i]=(a,b)
-    D[4]=p[1] 
-    for i in range(len(p[1])):
-        a,b=p[1][i]
+        p2[1][i]=(a,b)
+    D[4]=p2[1]
+    for i in range(len(piece[1])):
+        p3=piece.copy()
+        a,b=p3[1][i]
         a=-b
         b=a
-        p[1][i]=(a,b)   
-    D[5]=p[1]
+        p3[1][i]=(a,b)   
+    D[5]=p3[1]
     for i in range(len(v1[1])):
-        a,b=v1[1][i]
+        p4=v1.copy()
+        a,b=p4[1][i]
         a=b
         b=-a
-        v1[1][i]=(a,b)
-    D[6]=v1[1]
+        p4[1][i]=(a,b)
+    D[6]=p4[1]
     for i in range(len(v1[1])):
-        a,b=v1[1][i]
+        p5=v1.copy()
+        a,b=p5[1][i]
         a=-a
         b=-b
-        v1[1][i]=(a,b)
-    D[7]=v1[1]
+        p5[1][i]=(a,b)
+    D[7]=p5[1]
     for i in range(len(v1[1])):
-        a,b=v1[1][i]
+        p6=v1.copy()
+        a,b=p6[1][i]
         a=-b
         b=a
-        v1[1][i]=(a,b)
-    D[8]=v1[1]
+        p6[1][i]=(a,b)
+    D[8]=p6[1]
     for i in range(1,8):
         for j in range(i+1,9):
             if D[i]==D[j]:

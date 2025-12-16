9aa338b2d8536cc7a6f4588af01795a6e885576b
collecnoe <collec.noe@gmail.com>
2025-12-16 15:35:25 +0100

    Add files via upload

--- automatisation.py
+++ automatisation.py
@@ -1,68 +1,67 @@
+import bibliotheque_pieces as bb
+
 def retournement(piece):
-    retourne={1:[]}
+    retournement={1:[]}
     L=[]
+    M=[]
+    amax=0
+    bmax=0
     for i in range(len(piece[1])):
-        a,b=piece[1][i]
-        a=-a
-        L.append((a,b))
-    retourne[1]=L
-    return retourne
+        if piece[1][i][0]>amax:
+            amax=piece[1][i][0]
+        if piece[1][i][1]>bmax:
+            bmax=piece[1][i][1]
+    moy_a=amax/2
+    moy_b=bmax/2
+    for i in range(len(piece[1])):
+        L.append((piece[1][i][0]-moy_a,piece[1][i][1]-moy_b))
+        M.append((int(-L[i][0]+moy_a),int(L[i][1]+moy_b)))
+    retournement[1]=sorted(M)
+    return retournement
+
+def rotation(piece):
+    rotation={1:[]}
+    L=[]
+    M=[]
+    amax=0
+    bmax=0
+    for i in range(len(piece[1])):
+        if piece[1][i][0]>amax:
+            amax=piece[1][i][0]
+        if piece[1][i][1]>bmax:
+            bmax=piece[1][i][1]
+    moy_a=amax/2
+    moy_b=bmax/2
+    for i in range(len(piece[1])):
+        L.append((piece[1][i][0]-moy_a,piece[1][i][1]-moy_b))
+        M.append((int(L[i][1]+moy_b),int(-L[i][0]+moy_a)))
+    rotation[1]=sorted(M)
+    return rotation
 
 
 def versions(piece):
-    n=len(piece[1])
-    v1=retournement(piece)
-    D={1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
-    D[1]=piece[1].copy()
-    D[1]=sorted(D[1])
-    for i in range(n):
-        a,b=piece[1][i]
-        afin=b
-        b=-a
-        D[2].append((afin,b))
-    D[2]=sorted(D[2])
-    for i in range(n):
-        a,b=piece[1][i]
-        afin=-a
-        b=-b
-        D[3].append((afin,b))
-    D[3]=sorted(D[3])
-    for i in range(n):
-        a,b=piece[1][i]
-        afin=-b
-        b=a
-        D[4].append((afin,b))
-    D[4]=sorted(D[4])
-    D[5]=v1[1]
-    D[5]=sorted(D[5])
-    for i in range(n):
-        a,b=v1[1][i]
-        afin=b
-        b=-a
-        D[6].append((afin,b))
-    D[6]=sorted(D[6])
-    for i in range(n):
-        a,b=v1[1][i]
-        afin=-a
-        b=-b
-        D[7].append((afin,b))
-    D[7]=sorted(D[7])
-    for i in range(n):
-        a,b=v1[1][i]
-        afin=-b
-        b=a
-        D[8].append((afin,b))
-    D[8]=sorted(D[8])
-    for i in range(1,8):
+    r1=rotation(piece)
+    piece[2]=r1[1]     
+    r2=rotation(r1)
+    piece[3]=r2[1]
+    r3=rotation(r2)
+    piece[4]=r3[1]
+    ret=retournement(piece)
+    piece[5]=ret[1]
+    r11=rotation(ret)
+    piece[6]=r11[1]
+    r12=rotation(r11)
+    piece[7]=r12[1]
+    r13=rotation(r12)
+    piece[8]=r13[1]
+    for i in range(1,9):
         for j in range(i+1,9):
-            if D[i]==D[j]:
-                D[j]=[]
+            if piece[i]==piece[j]:
+                piece[j]=[]
     for k in range(1,9):
-        if D[k]==[]:
-            del D[k]
-    return D   
-        
-piece_17={1:[(0,0),(0,1),(0,2),(1,0),(2,0)]}
-print(versions(piece_17))
+        if piece[k]==[]:
+            del piece[k]
+    return piece
 
-
+pieces=[versions(bb.piece_1),versions(bb.piece_2),versions(bb.piece_3),versions(bb.piece_4),versions(bb.piece_5),versions(bb.piece_6),versions(bb.piece_7),versions(bb.piece_8),versions(bb.piece_9),versions(bb.piece_10),versions(bb.piece_11),versions(bb.piece_12),versions(bb.piece_13),versions(bb.piece_14),versions(bb.piece_15),versions(bb.piece_16),versions(bb.piece_17),versions(bb.piece_18),versions(bb.piece_19),versions(bb.piece_20),versions(bb.piece_21)]
+print(pieces)

211c0765a4ca601b9082ac436cb526503f782b6f
collecnoe <collec.noe@gmail.com>
2026-01-07 11:00:05 +0100

    Add files via upload

--- optimisation.py
+++ optimisation.py
@@ -1,4 +1,6 @@
 import game as g
+import math
+from copy import copy
 
 def heuristique_cases(game):
     L={}
@@ -47,3 +49,42 @@ def heuristique_bourrin(game):
     for elem in game.piecesPlayer[adversaire]:
         score+=len(elem[1])
     return score
+
+def minimax(game, heuristique, maximisant=True, profondeur=4):
+    i=0
+    for elem in game.piecesPlayer[game.player]:
+        for version in elem.values():
+            if game.gridListener.calc_possibilities(game.player,version)!=[]:
+                i=i+1
+                break
+        if i!=0:
+            break
+    if profondeur == 0 or i == 0:
+            return heuristique(game), None
+    adversaire = (game.player+1)%2
+    if maximisant:
+        meilleur_score = -math.inf
+        meilleur_coup = None
+        for elem in game.piecesPlayer[game.player]:
+            for version in elem.values():
+                for coup in game.gridListener.calc_possibilities(game.player,version):
+                    ng=copy(game)
+                    new_game = ng.gridListener.place_piece(version,coup,game.player)
+                    score, _ = minimax(new_game, adversaire, maximisant =False, profondeur = profondeur - 1)
+                    if score > meilleur_score:
+                        meilleur_score = score
+                        meilleur_coup = (coup,version,elem)
+        return meilleur_score, meilleur_coup
+    else:
+        pire_score = math.inf
+        meilleur_coup = None
+        for elem in game.piecesPlayer[adversaire]:
+            for version in elem.values():
+                for coup in game.gridListener.calc_possibilities(adversaire,version):
+                    ng=copy(game)
+                    new_game = ng.gridListener.place_piece(version,coup,adversaire)
+                    score, _ = minimax(new_game, adversaire, maximisant =True, profondeur = profondeur - 1)
+                    if score < pire_score:
+                        pire_score = score
+                        meilleur_coup = (coup,version,elem)
+        return pire_score, meilleur_coup

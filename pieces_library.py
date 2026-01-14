19f2ef7f09ae2e8d0b7ab26bcc419eccb5447827
Thib Erm <75995055+DevThib@users.noreply.github.com>
2026-01-14 17:05:31 +0100

    Update and rename bibliotheque_pieces.py to pieces_library.py

--- pieces_library.py
+++ pieces_library.py
@@ -1,6 +1,5 @@
 # Bibliothèque de pièces du jeu Blokus (21 pièces numérotées de 1 à 21 selon les règles du jeu)
 # Dictionnaire de tuples d'origine (0,0) en haut à gauche, coordonnées en (ligne,colonne)
-import matplotlib.pyplot as plt
 piece_1={1:[(0,0)]}
 piece_2={1:[(0,0),(1,0)]}
 piece_3={1:[(0,0),(1,0),(2,0)]}
@@ -22,6 +21,7 @@ piece_18={1:[(0,0),(0,1),(1,1),(1,2),(2,2)]}
 piece_19={1:[(0,0),(1,0),(1,1),(1,2),(2,2)]}
 piece_20={1:[(0,0),(1,0),(1,1),(1,2),(2,1)]}
 piece_21={1:[(0,1),(1,0),(1,1),(1,2),(2,1)]}
+
 
 
 

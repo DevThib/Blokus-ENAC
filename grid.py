77b34c5e5a1bba8447f543e5ab01103e21bf1d88
Thib Erm <75995055+DevThib@users.noreply.github.com>
2026-01-14 16:44:15 +0100

    Add files via upload

--- grid.py
+++ grid.py
@@ -12,7 +12,7 @@ class Grid:
         self.grid = self.build_grid()
         self.game = game
 
-    def build_grid(self):
+    def build_grid(self):#construction de la grille graphique
         container = QWidget()
         gridLayout = QGridLayout()
         container.setLayout(gridLayout)
@@ -25,23 +25,25 @@ class Grid:
                 gridLayout.addWidget(c.button,i,a)
                 self.cases.append(c)
         return container
-    def add_piece(self, piece,version, pos,player):
+    def add_piece(self, piece,version, pos,player):#ajout dune pièce sur la gille
         for t in piece[version]:
             if pos[0]+t[0] < self.width and pos[1]+t[1]< self.height:
                 case = self.cases[(pos[0] + t[0]) * self.height + (pos[1] + t[1])]
                 case.change_color(player)
                 case.clickable = False
         self.game.remove_piece_for_player(player, piece)
-
-    def get_coordinates(self,caseButton):
-        for i in range(len(self.cases)):
-            if self.cases[i].button == caseButton:
-                return (i//self.width,i%self.width)
-
-    def dark_border_case(self,pos):
+    def dark_border_case(self,pos):#surlignage de la case en noir (affichage des possibilités de jeu)
         self.cases[pos[0]*self.width+pos[1]].button.darken_border()
-    def grey_border_case(self,pos):
+    def grey_border_case(self,pos):#remettre la bordure de la case en gris
         self.cases[pos[0]*self.width+pos[1]].button.greyen_border()
+
+    def clean_possibilities(self):#retirer toutes les cases en surlignage noir
+        for c in self.cases:
+            if c.clickable:
+                c.button.greyen_border()
+    def show_possibilities(self,possibilities_list):#affichage des possibilités de jeu
+        for c in possibilities_list:
+            self.cases[c[0]*14+c[1]].button.darken_border()
 
 class Case:
     def __init__(self,x,y,grid):
@@ -52,7 +54,7 @@ class Case:
         self.clickable = True
 
     def create_button(self):
-        class CaseButton(QPushButton):
+        class CaseButton(QPushButton):#Le bouton est un objet créé par nous même car il doit hériter de QPushbutton pour avoir les évènements d'entrée et de sortie de la souris
             def __init__(self,case):
                 super().__init__()
                 self.clicked.connect(case.on_clicked)
@@ -66,7 +68,7 @@ class Case:
                                                          """)
 
                 self.case = case
-            def enterEvent(self, a0):
+            def enterEvent(self, a0):#lorsque la souris entre,nous surlignons en orange les cases correspondant à la pièce (prévisualisation d'un éventuel placement)
                 pos = (self.case.x,self.case.y)
                 for c in self.case.grid.game.selectedPiece.get_version():
                     if (pos[0]+c[0])*self.case.grid.width+pos[1]+c[1] < self.case.grid.height*self.case.grid.width:
@@ -79,7 +81,7 @@ class Case:
                                                                               }
                                                                     
                                                                                      """)
-            def leaveEvent(self, a0):
+            def leaveEvent(self, a0):#lorsque la souris sort,nous retirons ce surlignage en orange
                 pos = (self.case.x,self.case.y)
                 for c in self.case.grid.game.selectedPiece.get_version():
                     if (pos[0]+c[0])*self.case.grid.width+pos[1]+c[1] < self.case.grid.height*self.case.grid.width:
@@ -88,7 +90,7 @@ class Case:
                             cb.button.darken_border()
                         else:
                             cb.button.greyen_border()
-            def darken_border(self):
+            def darken_border(self):#mise de la bordure en noir
                 if self.case.clickable:
                     self.setStyleSheet("""
                                                 QPushButton {
@@ -98,7 +100,7 @@ class Case:
     
                                                                                                              """)
 
-            def greyen_border(self):
+            def greyen_border(self):#mise de la bourdure en gris
                 if self.case.clickable:
                     self.setStyleSheet("""
                                                 QPushButton {
@@ -110,14 +112,13 @@ class Case:
 
         return CaseButton(self)
 
-    def on_clicked(self):
-        if self.grid.game.place_piece((self.x, self.y)):
+    def on_clicked(self):#fonction éxécutée quand on clique sur une case
+        if self.grid.game.place_piece((self.x, self.y)):#si jeu possible alors on ajoute la pièce à la grille et on change de joueur
             self.grid.add_piece(self.grid.game.selectedPiece.piece, self.grid.game.selectedPiece.version + 1,(self.x, self.y), self.grid.game.player)
             self.grid.game.change_player()
-            self.grid.game.selectedPiece.show_possibilities(self.grid.game.bot)
             if self.grid.game.bot:
                 self.grid.game.bot_play()
-    def change_color(self,player):
+    def change_color(self,player):#fonction qui met la case en vert ou rouge quand un joueur a joué
         if player == 0:
             self.button.setCursor(Qt.CursorShape.ArrowCursor)
             self.button.setStyleSheet("""
